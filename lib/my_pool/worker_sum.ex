3d162548e8492ffc42df0dd2a1ee88a3898c7a31
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-03 13:03:25 -0500

    Workers for Sum and Sub

---
+++ lib/my_pool/worker_sum.ex
@@ -0,0 +1,16 @@
+defmodule MyPool.Worker.Sum do
+  use GenServer
+
+  def start_link([]), do: GenServer.start_link(__MODULE__, [])
+
+  @impl true
+  def init(_) do
+    {:ok, []}
+  end
+
+  @impl true
+  def handle_call({:operation, a, b}, _from, state) do
+     IO.inspect "SUM VALUES #{a}, #{b} WAS PROCESSED BY #{inspect self()}"
+    {:reply, {:ok, a + b}, [a+b]}
+  end
+end
