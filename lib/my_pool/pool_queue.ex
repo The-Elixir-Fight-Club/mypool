3758dc398f3e7ff2e5709e69c20647ae1051e63f
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-03 12:02:44 -0500

    Add callbacks

--- lib/my_pool/pool_queue.ex
+++ lib/my_pool/pool_queue.ex
@@ -10,4 +10,13 @@ defmodule MyPool.PoolQueue do
     # our queue when our process start is always empty it means an empty list
     {:ok, []}
   end
+
+  @impl true
+  def handle_call(:get, _from, queue), do: {:reply, {:ok, queue}, queue}
+
+  @impl true
+  def handle_call(:get_pid, _from, [pid | queue]), do: {:reply, {:ok, pid}, queue ++ [pid]}
+
+  @impl true
+  def handle_cast({:in, pid}, queue), do: {:noreply, queue ++ [pid]}
 end
